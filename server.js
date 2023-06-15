17dea4c3418a0805ae3d6468de8c7eca5a27a0bf
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-06-15 15:29:46 -0400

    Created Routes Without Router

--- server.js
+++ server.js
@@ -9,6 +9,28 @@ const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Successfully Connected to Database"));
 
+app.use(express.json());
+// will use router later
+
+// Getting All
+app.get("/subscribers", (request, response) => {
+    response.send({ message: "Hello World" });
+});
+
+// Getting One
+app.get("/subscribers/:id", (request, response) => {
+    response.send({ name: request.params.id });
+});
+
+// Creating One
+app.post("/subscribers", (request, response) => {});
+
+// Updating One
+app.patch("/subscribers/:id", (request, response) => {});
+
+// Deleting One
+app.delete("/subscribers/:id", (request, response) => {});
+
 app.listen(PORT, () => {
     console.log(`Server Started on Port ${PORT}`);
 });
